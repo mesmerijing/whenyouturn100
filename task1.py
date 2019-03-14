157e1a4e732d63461d84176477ae96cab89016ee
mesmerijing <47663572+mesmerijing@users.noreply.github.com>
2019-03-14 22:29:13 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+﻿name=input("Give your name ")
+print ("Your name is "+ name)
+age=int(input("Give your age "))
+gap = 100-age
+x=(name + " you will turn 100 in " +str(gap)+ " years!!")
+print (x)
+y=x.strip()
+num=int(input("Give me another number "))
+for i in range (num):
+ print (y)
